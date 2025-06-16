935f778c17892a746f6fde6976c61ebed91e9478
SimoooWann <simooowann@gmail.com>
2025-06-16 17:20:41 +0800

    修复部分问题

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -169,7 +169,7 @@ function App() {
           "Content-Type": "application/json",
         },
         
-        body: JSON.stringify({ source_text: data.text, image_url:files?.[0].ID ? `/api/files/${files?.[0].ID}`:undefined }),
+        body: JSON.stringify({ source_text: data.text, image_url:files?.[0]?.ID ? `${location.origin}/api/files/${files?.[0].ID}`:undefined }),
         onMessage(data: EventData<{ text?: string; message?: string }>) {
           switch (data.type) {
             case "chunk":
